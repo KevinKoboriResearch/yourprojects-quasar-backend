121fd40cf71ee59204b7d6bbb8146018816a434e
kevinkobori <kevin.kobori@hotmail.com>
2020-09-02 00:19:42 -0300

    user articles insert ok

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,8 +2,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 module.exports = app => {
-    app.use(bodyParser.json())
-    // app.use(bodyParser.json({limit: '50mb', extended: true}))
-    // app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+    // app.use(bodyParser.json())
+    app.use(bodyParser.json({limit: '50mb', extended: true}))
+    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
     app.use(cors())
 }
